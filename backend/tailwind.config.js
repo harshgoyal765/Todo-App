cdc98bfeb8ad660b74b4efe52d3765c38b7da3d2
harsh goyal <18harshgoyal@gmail.com>
2025-07-12 21:30:21 +0530

    addfiles

---
+++ backend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,ejs,js}"],
+  // content:[ "./views/*.ejs",        // for all EJS templates
+  //   "./public/**/*.html",      // if you use any HTML in public
+  //   "./*.ejs",                 // root-level EJS if any
+  //   "./**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
